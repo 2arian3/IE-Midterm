2564b6258b4d6dc215a163c4e4ea92a99acbe2bb
2arian3 <aryan.boukani2@gmail.com>
2021-12-01 17:10:56 +0330

    Added calling api functionality.

--- js/main.js
+++ js/main.js
@@ -4,18 +4,37 @@ const handleRequest = (name) => {
     fetch(URL + `?name=${name}`)
     .then(response => response.json())
     .then(function(response){
-        console.log(response);
+        document.getElementById('gender').innerHTML = response.gender;
+        document.getElementById('probability').innerHTML = response.probability;
+        response.gender === 'male' ? document.getElementById('male').checked = true : document.getElementById('female').checked = true;
     })
     .catch(error => {
-
+        document.getElementById('error-message').style.visibility = 'visible';
+        document.getElementById('error').innerHTML = error;
+        setTimeout(() => {
+            document.getElementById('error-message').style.visibility = 'hidden';
+        }, 2000);
     });
+}
+
+function getNamesObject() {
+    return localStorage.getItem('names') ? localStorage.getItem('names') : [];
+}
+
+function setNamesObject(names) {
+    localStorage.setItem('names', names);
 }
 
 function OnSubmit(e) {
     e.preventDefault();
+    const name = document.getElementById('name').value;
+    name ? handleRequest(name) : undefined;
 }
 
 function OnSave(e) {
+    if (document.getElementById('name') !== '') {
+
+    }
 }
 
 function OnClear(e) {
