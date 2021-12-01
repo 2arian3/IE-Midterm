4c0329091ba3ce7a15c4e6b5f8e2fc567dc7f006
2arian3 <aryan.boukani2@gmail.com>
2021-12-01 17:37:36 +0330

    Completed js functions and modified the styles.

--- js/main.js
+++ js/main.js
@@ -6,7 +6,16 @@ const handleRequest = (name) => {
     .then(function(response){
         document.getElementById('gender').innerHTML = response.gender;
         document.getElementById('probability').innerHTML = response.probability;
-        response.gender === 'male' ? document.getElementById('male').checked = true : document.getElementById('female').checked = true;
+        response.gender === 'male' ? 
+        document.getElementById('male').checked = true : 
+        response.gender === 'female' ? document.getElementById('female').checked = true : '';
+
+        const names = getNamesObject();
+        if (names.hasOwnProperty(name)) {
+            document.getElementById('saved').innerHTML = names[name];
+        } else {
+            document.getElementById('saved').innerHTML = '';
+        }
     })
     .catch(error => {
         document.getElementById('error-message').style.visibility = 'visible';
@@ -18,11 +27,11 @@ const handleRequest = (name) => {
 }
 
 function getNamesObject() {
-    return localStorage.getItem('names') ? localStorage.getItem('names') : [];
+    return localStorage.getItem('names') ? JSON.parse(localStorage.getItem('names')) : {};
 }
 
 function setNamesObject(names) {
-    localStorage.setItem('names', names);
+    localStorage.setItem('names', JSON.stringify(names));
 }
 
 function OnSubmit(e) {
@@ -32,10 +41,24 @@ function OnSubmit(e) {
 }
 
 function OnSave(e) {
-    if (document.getElementById('name') !== '') {
-
+    const name = document.getElementById('name').value;
+    if (name !== '') {
+        const names = getNamesObject();
+        if (document.getElementById('male').checked) {
+            names[name] = 'male';
+        } else if (document.getElementById('female').checked) {
+            names[name] = 'female';
+        }
+        setNamesObject(names);
     }
 }
 
 function OnClear(e) {
+    const name = document.getElementById('name').value;
+    const names = getNamesObject();
+    if (name !== '' && names.hasOwnProperty(name)) {
+        delete names[name];
+        setNamesObject(names);
+        document.getElementById('saved').innerHTML = '';
+    }
 }
